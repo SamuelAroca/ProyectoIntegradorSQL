e60c514860e2de81dfcfcd95d52166e9e58c2c5c
SamuelAroca <samuel.arocad@unac.edu.co>
2022-12-16 23:12:16 -0500

    Base de datos actualizada

--- DB/prointegrador.sql
+++ DB/prointegrador.sql
@@ -11,14 +11,15 @@ CREATE TABLE `admins` (
  
  CREATE TABLE `productos` (
    `id` int NOT NULL AUTO_INCREMENT,
-   `codigo` varchar(20) NOT NULL,
+   `id_producto` varchar(20) NOT NULL,
    `nombre` varchar(50) NOT NULL,
    `precio` decimal(10,0) NOT NULL,
    `cantidad` int NOT NULL,
+   `id_proveedor` varchar(50) NOT NULL,
    PRIMARY KEY (`id`),
-   UNIQUE KEY `codigo_UNIQUE` (`codigo`),
+   UNIQUE KEY `codigo_UNIQUE` (`id_producto`),
    UNIQUE KEY `nombre_UNIQUE` (`nombre`)
- ) ENGINE=InnoDB AUTO_INCREMENT=24 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+ ) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
  
  CREATE TABLE `users` (
    `id` int NOT NULL AUTO_INCREMENT,
@@ -29,14 +30,23 @@ CREATE TABLE `admins` (
    UNIQUE KEY `identificacion_UNIQUE` (`identificacion`)
  ) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
  
- CREATE TABLE `proveedores` (
+ CREATE TABLE `proveedor` (
    `id` int NOT NULL AUTO_INCREMENT,
-   `codigo` varchar(45) NOT NULL,
-   `nombre` varchar(45) NOT NULL,
-   `producto` varchar(45) NOT NULL,
-   `precio` varchar(45) NOT NULL,
-   `cantidad` varchar(45) NOT NULL,
+   `id_proveedor` varchar(50) NOT NULL,
+   `nombre_proveedor` varchar(50) NOT NULL,
+   `tipo_producto` varchar(50) NOT NULL,
    PRIMARY KEY (`id`),
-   UNIQUE KEY `codigo_UNIQUE` (`codigo`),
-   UNIQUE KEY `producto_UNIQUE` (`producto`)
- ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+   UNIQUE KEY `id_proveedor_UNIQUE` (`id_proveedor`),
+   UNIQUE KEY `nombre_proveedor_UNIQUE` (`nombre_proveedor`)
+ ) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+ 
+ CREATE TABLE `carrito` (
+   `id` int NOT NULL AUTO_INCREMENT,
+   `id_producto` varchar(50) NOT NULL,
+   `nombre` varchar(50) NOT NULL,
+   `precio` decimal(10,0) NOT NULL,
+   `cantidad` int NOT NULL,
+   PRIMARY KEY (`id`),
+   UNIQUE KEY `id_producto_UNIQUE` (`id_producto`),
+   UNIQUE KEY `nombre_UNIQUE` (`nombre`)
+ ) ENGINE=InnoDB AUTO_INCREMENT=28 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
