3c416d52489131dca040adfe0e3444bf58fcc6da
SamuelAroca <samuel.arocad@unac.edu.co>
2022-12-23 02:11:06 -0500

    Mejora grafica y Añadida opcion de ver y actualizar saldo

--- src/principal/administracion/Tienda/Tienda.java
+++ src/principal/administracion/Tienda/Tienda.java
@@ -1,8 +1,8 @@
 package principal.administracion.Tienda;
 
 import java.awt.*;
-import java.awt.event.*;
 import java.sql.*;
+import java.util.*;
 import javax.swing.*;
 import principal.administracion.Tienda.panelsShop.*;
 import principal.logANDres.*;
@@ -11,9 +11,14 @@ public class Tienda extends javax.swing.JFrame {
 
     public Tienda() {
         initComponents();
+        mostrarSaldo();
+        cbxSaldo.setSelectedIndex(0);
+        btnAgregarSaldo.setVisible(false);
+        txtSaldo.setEditable(false);
+        txtAñadirSaldo.setVisible(false);
+        setDefaultCloseOperation(DO_NOTHING_ON_CLOSE);
         PanelTienda pt = new PanelTienda();
         showPanel(pt);
-        cerrar();
     }
 
     @SuppressWarnings("unchecked")
@@ -22,13 +27,24 @@ public class Tienda extends javax.swing.JFrame {
 
         productos = new javax.swing.JButton();
         carrito = new javax.swing.JButton();
+        compras = new javax.swing.JButton();
         salir = new javax.swing.JButton();
+        jLabel1 = new javax.swing.JLabel();
+        txtSaldo = new javax.swing.JTextField();
+        txtAñadirSaldo = new javax.swing.JTextField();
+        cbxSaldo = new javax.swing.JComboBox<>();
+        btnAgregarSaldo = new javax.swing.JButton();
         butonRegresar = new javax.swing.JButton();
         labelLogo = new javax.swing.JLabel();
         rectangulo = new javax.swing.JLabel();
         content = new javax.swing.JPanel();
 
         setBackground(new java.awt.Color(255, 255, 255));
+        addWindowListener(new java.awt.event.WindowAdapter() {
+            public void windowClosing(java.awt.event.WindowEvent evt) {
+                formWindowClosing(evt);
+            }
+        });
         getContentPane().setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
 
         productos.setIcon(new javax.swing.ImageIcon(getClass().getResource("/principal/icons/tienda/BtnProductos.png"))); // NOI18N
@@ -53,6 +69,17 @@ public class Tienda extends javax.swing.JFrame {
         });
         getContentPane().add(carrito, new org.netbeans.lib.awtextra.AbsoluteConstraints(60, 350, -1, -1));
 
+        compras.setIcon(new javax.swing.ImageIcon(getClass().getResource("/principal/icons/tienda/BtnCompras.png"))); // NOI18N
+        compras.setBorder(null);
+        compras.setBorderPainted(false);
+        compras.setContentAreaFilled(false);
+        compras.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                comprasActionPerformed(evt);
+            }
+        });
+        getContentPane().add(compras, new org.netbeans.lib.awtextra.AbsoluteConstraints(60, 450, -1, -1));
+
         salir.setIcon(new javax.swing.ImageIcon(getClass().getResource("/principal/icons/tienda/energia.png"))); // NOI18N
         salir.setBorder(null);
         salir.setBorderPainted(false);
@@ -63,6 +90,31 @@ public class Tienda extends javax.swing.JFrame {
             }
         });
         getContentPane().add(salir, new org.netbeans.lib.awtextra.AbsoluteConstraints(220, 730, -1, 20));
+
+        jLabel1.setIcon(new javax.swing.ImageIcon(getClass().getResource("/principal/icons/tienda/saldo.png"))); // NOI18N
+        getContentPane().add(jLabel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(80, 560, -1, -1));
+        getContentPane().add(txtSaldo, new org.netbeans.lib.awtextra.AbsoluteConstraints(50, 610, 80, -1));
+        getContentPane().add(txtAñadirSaldo, new org.netbeans.lib.awtextra.AbsoluteConstraints(50, 610, 80, -1));
+
+        cbxSaldo.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "Saldo", "Agregar Saldo" }));
+        cbxSaldo.addItemListener(new java.awt.event.ItemListener() {
+            public void itemStateChanged(java.awt.event.ItemEvent evt) {
+                cbxSaldoItemStateChanged(evt);
+            }
+        });
+        getContentPane().add(cbxSaldo, new org.netbeans.lib.awtextra.AbsoluteConstraints(140, 610, -1, -1));
+
+        btnAgregarSaldo.setIcon(new javax.swing.ImageIcon(getClass().getResource("/principal/icons/tienda/plus-pequeno.png"))); // NOI18N
+        btnAgregarSaldo.setToolTipText("Agregar Saldo");
+        btnAgregarSaldo.setBorder(null);
+        btnAgregarSaldo.setBorderPainted(false);
+        btnAgregarSaldo.setContentAreaFilled(false);
+        btnAgregarSaldo.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnAgregarSaldoActionPerformed(evt);
+            }
+        });
+        getContentPane().add(btnAgregarSaldo, new org.netbeans.lib.awtextra.AbsoluteConstraints(30, 610, 20, 20));
 
         butonRegresar.setBackground(new java.awt.Color(235, 197, 30));
         butonRegresar.setIcon(new javax.swing.ImageIcon(getClass().getResource("/principal/icons/tienda/regresar.png"))); // NOI18N
@@ -107,7 +159,7 @@ public class Tienda extends javax.swing.JFrame {
 
     //Regresa al Login borrando el archivo carrito.txt
     private void butonRegresarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_butonRegresarActionPerformed
-        confirmarRegresar();
+        regresar();
         eliminarUsuarioActual();
         dispose();
         JFrame frameMain = new Login();
@@ -118,6 +170,108 @@ public class Tienda extends javax.swing.JFrame {
         frameMain.setVisible(true);
     }//GEN-LAST:event_butonRegresarActionPerformed
 
+    private void formWindowClosing(java.awt.event.WindowEvent evt) {//GEN-FIRST:event_formWindowClosing
+        cerrar();
+    }//GEN-LAST:event_formWindowClosing
+
+    private void comprasActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_comprasActionPerformed
+        Compras cr = new Compras();
+        showPanel(cr);
+    }//GEN-LAST:event_comprasActionPerformed
+
+    private void btnAgregarSaldoActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnAgregarSaldoActionPerformed
+        if (validarSaldo(txtAñadirSaldo.getText()) && !txtAñadirSaldo.getText().isEmpty()) {    
+            try {
+                PreparedStatement ps = null;
+                ResultSet rs = null;
+
+                Conexion objCon = new Conexion();
+                Connection con = objCon.getConection();
+
+                ps = con.prepareStatement("SELECT id_usuario FROM usuario_actual");
+                rs = ps.executeQuery();
+
+                while (rs.next()) {
+                    int idUsuario = rs.getInt(1);
+                    int saldo = Integer.parseInt(txtAñadirSaldo.getText());
+
+                    PreparedStatement ps1 = null;
+                    ResultSet rs1 = null;
+
+                    ps1 = con.prepareStatement("SELECT saldo FROM users WHERE identificacion = ?");
+                    ps1.setInt(1, idUsuario);
+                    rs1 = ps1.executeQuery();
+                    rs1.next();
+                    int saldoUser = rs1.getInt(1);
+                    saldo += saldoUser;
+
+                    PreparedStatement ps2 = null;
+
+                    ps2 = con.prepareStatement("UPDATE users SET saldo = ? WHERE identificacion = ?");
+                    ps2.setInt(1, saldo);
+                    ps2.setInt(2, idUsuario);
+                    int res = ps2.executeUpdate();
+
+                    if (res > 0) {
+                        JOptionPane.showMessageDialog(null, "Dinero agregado");
+                        cbxSaldo.setSelectedIndex(0);
+                        txtAñadirSaldo.setText(null);
+                        mostrarSaldo();
+                        txtSaldo.setVisible(true);
+                        btnAgregarSaldo.setVisible(false);
+                    } else {
+                        JOptionPane.showMessageDialog(null, "Error al agregar dinero");
+                    }
+                }
+            } catch (SQLException e) {
+                System.out.println("Error: " + e);
+            }
+        } else {
+            JOptionPane.showMessageDialog(null, "Verifique o agregue el saldo");
+        }
+    }//GEN-LAST:event_btnAgregarSaldoActionPerformed
+
+    private void cbxSaldoItemStateChanged(java.awt.event.ItemEvent evt) {//GEN-FIRST:event_cbxSaldoItemStateChanged
+        if (Objects.equals(cbxSaldo.getSelectedItem(), "Saldo")) {
+            mostrarSaldo();
+            txtAñadirSaldo.setVisible(false);
+            txtSaldo.setVisible(true);
+            btnAgregarSaldo.setVisible(false);
+        } else if (Objects.equals(cbxSaldo.getSelectedItem(), "Agregar Saldo")) {
+            txtSaldo.setVisible(false);
+            txtAñadirSaldo.setVisible(true);
+            btnAgregarSaldo.setVisible(true);
+        }
+    }//GEN-LAST:event_cbxSaldoItemStateChanged
+    
+    private void mostrarSaldo() {
+        try {
+            PreparedStatement ps = null;
+            ResultSet rs = null;
+            
+            Conexion objCon = new Conexion();
+            Connection con = objCon.getConection();
+            
+            ps = con.prepareStatement("SELECT id_usuario FROM usuario_actual");
+            rs = ps.executeQuery();
+            
+            while (rs.next()) {
+                int id = rs.getInt(1);
+                
+                PreparedStatement ps1 = null;
+                ResultSet rs1 = null;
+                
+                ps1 = con.prepareStatement("SELECT saldo FROM users WHERE identificacion = ?");
+                ps1.setInt(1, id);
+                rs1 = ps1.executeQuery();
+                rs1.next();
+                String saldo = rs1.getString(1);
+                txtSaldo.setText(saldo);
+            }
+        } catch (SQLException e) {
+            System.out.println("Error: " + e);
+        }
+    }
     //Cambia los paneles de la tienda
     private void showPanel(JPanel p) {
         p.setSize(1078, 767);
@@ -131,42 +285,27 @@ public class Tienda extends javax.swing.JFrame {
 
     //Metodo para borrar o no el carrito
     private void cerrar() {
-        try {
-            this.setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE);
-            addWindowListener(new WindowAdapter() {
-                @Override
-                public void windowClosing(WindowEvent e) {
-                    eliminarUsuarioActual();
-                    confirmarSalida();
-                }
-            });
-            this.setVisible(true);
-        } catch (Exception e) {
-            JOptionPane.showMessageDialog(null, e.getMessage());
-        }
-    }
+        String botones[] = {"Cerrar", "Cancelar"};
+        int eleccion = JOptionPane.showOptionDialog(this, "¿Desea cerrar la aplicacion?", "Titulo", 0, 0, null, botones, this);
 
-    //Confirma la salida del usuario
-    public void confirmarSalida() {
-        int valor = JOptionPane.showConfirmDialog(this, "¿Esta seguro de cerrar la aplicación?", "Advertencia", JOptionPane.YES_NO_OPTION);
-
-        if (valor == JOptionPane.YES_OPTION) {
+        if (eleccion == JOptionPane.YES_OPTION) {
+            eliminarUsuarioActual();
             eliminarCarrito();
             System.exit(0);
-        } else if (valor == JOptionPane.NO_OPTION) {
-            PanelTienda pt = new PanelTienda();
-            showPanel(pt);
+        } else if (eleccion == JOptionPane.NO_OPTION) {
+            System.out.println("Se cancelo el cierre");
         }
     }
 
-    public void confirmarRegresar() {
-        int valor = JOptionPane.showConfirmDialog(this, "¿Esta seguro de cerrar la sesion?", "Advertencia", JOptionPane.YES_NO_OPTION);
+    private void regresar() {
+        String botones[] = {"Cerrar", "Cancelar"};
+        int eleccion = JOptionPane.showOptionDialog(this, "¿Desea cerrar la aplicacion?", "Titulo", 0, 0, null, botones, this);
 
-        if (valor == JOptionPane.YES_OPTION) {
+        if (eleccion == JOptionPane.YES_OPTION) {
+            eliminarUsuarioActual();
             eliminarCarrito();
-        } else if (valor == JOptionPane.NO_OPTION) {
-            PanelTienda pt = new PanelTienda();
-            showPanel(pt);
+        } else if (eleccion == JOptionPane.NO_OPTION) {
+            System.out.println("Se cancelo el cierre");
         }
     }
 
@@ -253,13 +392,23 @@ public class Tienda extends javax.swing.JFrame {
             System.out.println("Error 1: " + e);
         }
     }
+    
+    public static boolean validarSaldo(String precio) {
+        return precio.matches("^[0-9]{1}[0-9]+$");
+    }
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton btnAgregarSaldo;
     private javax.swing.JButton butonRegresar;
     private javax.swing.JButton carrito;
+    private javax.swing.JComboBox<String> cbxSaldo;
+    private javax.swing.JButton compras;
     private javax.swing.JPanel content;
+    private javax.swing.JLabel jLabel1;
     public javax.swing.JLabel labelLogo;
     private javax.swing.JButton productos;
     private javax.swing.JLabel rectangulo;
     private javax.swing.JButton salir;
+    private javax.swing.JTextField txtAñadirSaldo;
+    private javax.swing.JTextField txtSaldo;
     // End of variables declaration//GEN-END:variables
 }
