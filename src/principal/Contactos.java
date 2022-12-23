d83ec6571d06b94110ca75698df35d1a8b31da5c
SamuelAroca <samuel.arocad@unac.edu.co>
2022-12-23 02:18:22 -0500

    Correcion minima

--- src/principal/Contactos.java
+++ src/principal/Contactos.java
@@ -10,7 +10,6 @@ public class Contactos extends javax.swing.JFrame {
     public Contactos() {
         initComponents();
     }
-
     @SuppressWarnings("unchecked")
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
